10b7411668f77bfccd7c0e7bd6af30becc6d6e8a
horrificsquid <128898152+horrificsquid@users.noreply.github.com>
2023-03-25 10:18:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTA4ODE4NTgzMjExODE3Mzc2Ng.GcDXxB.Xoaj_G3bcWaJasovyUAQMKOliEOd56oy0D_Ufo'
+    TOKEN = ''
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
